f4ddf02b87b7f6989c4523f9470d84aadecb9a7f
pavel <pavel@DESKTOP-EDGG59N>
2022-06-08 17:34:04 +0300

    Add project files.

---
+++ WordsList/Dictionary.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace WordsList
+{
+    class MyDictionary
+    {
+        SortedList mySL = new SortedList();
+
+        public string Name { get; set; }
+
+        public MyDictionary() { }
+
+        public MyDictionary(string n)
+        {
+            Name = n;
+        }
+
+        public void Add()
+        {
+            string str1, str2;
+
+            Write("Слово - ");
+            str1 = ReadLine();
+            Write("Перевод - ");
+            str2 = ReadLine();
+
+            mySL.Add(str1, str2);
+        }
+
+        public void Search()
+        {
+            string str1;
+            int ch = 0;
+            Write("Поиск по 1 - слову или 2- переводу?");
+            ch = Convert.ToInt32(ReadLine());
+            if (ch == 1)
+            {
+                Write("Слово - ");
+                str1 = ReadLine();
+            }
+            else
+            {
+                Write("Перевод - ");
+                str1 = ReadLine();
+            }
+
+            for (int i = 0; i < mySL.Count; i++)
+                if (mySL.GetKey(i) == str1)
+                    WriteLine($"\t{mySL.GetKey(i)}:\t{mySL.GetByIndex(i)}");
+        }
+
+        public void Redact()
+        {
+            string str1, str2;
+            int ch = 0;
+            Write("Поиск по 1 - слову или 2- переводу?");
+            ch = Convert.ToInt32(ReadLine());
+            if (ch == 1)
+            {
+                Write("Слово - ");
+                str1 = ReadLine();
+            }
+            else
+            {
+                Write("Перевод - ");
+                str1 = ReadLine();
+            }
+
+            for (int i = 0; i < mySL.Count; i++)
+                if (mySL.GetKey(i) == str1)
+                {
+                    WriteLine($"\t{mySL.GetKey(i)}:\t{mySL.GetByIndex(i)}");
+                    Write($"Заменить на ? ");
+                    str2 = ReadLine();
+                    mySL.SetByIndex(mySL.IndexOfKey(i), str2);
+                }
+        }
+
+        public void DeleteWord()
+        {
+            string str1;
+
+            Write("Слово - ");
+            str1 = ReadLine();
+
+            mySL.Remove(str1);
+
+            WriteToFile();
+        }
+
+        public void Clear()
+        {
+            mySL.Clear();
+        }
+
+        public void Print()
+        {
+            WriteLine("\t-Слово-\t-Перевод-");
+            for (int i = 0; i < mySL.Count; i++)
+                WriteLine($"\t{mySL.GetKey(i)}:\t{mySL.GetByIndex(i)}");
+            WriteLine();
+        }
+          
+        public void WriteToFile()
+        {
+            StreamWriter sw = new StreamWriter($"{Name}_D.txt", true);
+            string line;
+
+            // Считываем строку с клавиатуры
+            line = "\t-Слово-\t-Перевод-";
+            // Записываем строку в файл
+            sw.WriteLine(line);
+
+            for (int i = 0; i < mySL.Count; i++)
+            {
+                line = $"\t{mySL.GetKey(i)}:\t{mySL.GetByIndex(i)}";
+                sw.WriteLine(line);
+            }
+            sw.Close();
+        }
+
+        public void ReadInFile()
+        {
+            StreamReader sr = new StreamReader("Dictionary.txt", Encoding.UTF8);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                WriteLine(line);
+                Thread.Sleep(100);
+            }
+            sr.Close();
+        }
+    }
+} 
---
+++ WordsList/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+
+namespace WordsList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary[] AllD = new MyDictionary[255];
+
+            int flag = 0;
+            while (flag == 0)
+            {
+                int choise = 0;
+                WriteLine("1. Создать словарь\n2. Открыть словарь\n3. Выйти");
+                choise = Convert.ToInt32(ReadLine());
+
+                switch (choise) 
+                {
+                    case 1:
+                        MyDictionary D = new MyDictionary();
+                        string ch;
+                        WriteLine("Язык ?");
+                        ch = ReadLine();
+                        D.Name = ch;
+                        for (int i = 0; i < AllD.Length; i++)
+                        {
+                            if (AllD[i] == null)
+                            {
+                                AllD[i] = D;
+                                break;
+                            }
+                        } 
+                        break;
+
+                    case 2: 
+                        int cho = 0;
+                        for (int i = 0; i < AllD.Length; i++)
+                        {
+                            if (AllD[i] == null) break; 
+                            WriteLine($"{i + 1}. {AllD[i].Name}"); 
+                        } 
+                        cho = Convert.ToInt32(ReadLine());
+
+                        int f = 0;
+                        while (f == 0)
+                        {
+                            int chois = 0;
+                            WriteLine("1. Добавить слово \n" +
+                                "2. Удалить слово \n" +
+                                "3. Поиск \n" +
+                                "4. Редактировать слово \n" +
+                                "5. Удалить все слова \n" +
+                                "6. Показать все слова \n" + 
+                                "7. Выход ");
+                            chois = Convert.ToInt32(ReadLine());
+
+                            switch (chois)
+                            {
+                                case 1: AllD[cho - 1].Add(); break;
+                                case 2: AllD[cho - 1].DeleteWord(); break;
+                                case 3: AllD[cho - 1].Search(); break;
+                                case 4: AllD[cho - 1].Redact(); break;
+                                case 5: AllD[cho - 1].Clear(); break;
+                                case 6: AllD[cho - 1].Print(); break; 
+                                case 7:
+                                    AllD[cho - 1].WriteToFile();
+                                    f++; 
+                                    break;
+                                default: WriteLine("Try again"); break;
+                            }
+                        } 
+                        break;
+
+                    case 3: Environment.Exit(0); break;  
+                    default: WriteLine("Try again"); break;
+                }
+            } 
+        }
+    } 
+}
